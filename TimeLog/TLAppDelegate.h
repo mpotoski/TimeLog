53882ad9d41ee4228c867498bdc1ca4e515b384d
Megan Potoski <mpotoski@stanford.edu>
2013-08-24 16:00:18 -0700

    Initial Commit

---
+++ TimeLog/TLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAppDelegate.h
+//  TimeLog
+//
+//  Created by Megan Potoski on 2013-08-24.
+//  Copyright (c) 2013 Megan Potoski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimeLog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeLog
+//
+//  Created by Megan Potoski on 2013-08-24.
+//  Copyright (c) 2013 Megan Potoski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+  }
+}
